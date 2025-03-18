4deca821d406c17ab9c67af191f56c1f451c8feb
JonathasSL <jonathasbent@gmail.com>
2025-03-18 18:42:02 -0300

    feature: UserEntityTests

---
+++ Vestis.03-Domain.tests/Tests/UserEntityTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Vestis._03_Domain.tests.Tests;
+
+public class UserEntityTests
+{
+    [Fact]
+    public void ShouldChangeName()
+    {
+        // Arrange
+        var user = new UserEntityFaker().Generate();
+        var newName = new Faker().Name.FullName();
+
+        // Act
+        user.ChangeName(newName);
+
+        // Assert
+        Assert.Equal(newName, user.Name);
+    }
+
+    [Fact]
+    public void ShouldChangeEmail()
+    {
+        //Arrange
+        var user = new UserEntityFaker().Generate();
+        var newEmail = new Faker().Internet.Email();
+
+        //Act
+        user.ChangeEmail(newEmail);
+
+        //Assert
+        Assert.Equal(newEmail, user.Email);
+    }
+
+    [Fact]
+    public void ShouldChangePassword()
+    {
+        //Arrange
+        var user = new UserEntityFaker().Generate();
+        var newPassword = new Faker().Internet.Password();
+        
+        //Act
+        user.ChangePassword(newPassword);
+        
+        //Assert
+        Assert.Equal(newPassword, user.Password);
+    }
+}
