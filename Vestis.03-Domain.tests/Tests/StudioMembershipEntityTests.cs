6d60bf8ff4d7bce589eb8d5595748e6b04882f53
JonathasSL <jonathasbent@gmail.com>
2025-03-20 22:34:49 -0300

    feature: StudioMembershipEntityTests

---
+++ Vestis.03-Domain.tests/Tests/StudioMembershipEntityTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vestis._03_Domain.tests.Tests;
+
+public class StudioMembershipEntityTests
+{
+    [Fact]
+    public void ChangeRole_ShouldChangeRole()
+    {
+        // Arrange
+        var membership = new StudioMembershipEntityFaker().Generate();
+        var newRole = new RoleEntityFaker().Generate();
+
+        //Act
+        membership.ChangeRole(newRole);
+
+        //Assert
+        Assert.Equal(newRole, membership.Role);
+    }
+}
