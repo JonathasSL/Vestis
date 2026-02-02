28c4f8988742577e5a5a1436b3065cf3c8d78d79
JonathasSL <jonathasbent@gmail.com>
2026-02-02 19:24:41 -0300

    feat: Product configuration

---
+++ Vestis._04_Infrastructure/Mapping/ProductEntityConfiguration.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vestis._03_Domain.Entities;
+
+namespace Vestis._04_Infrasctructure.Mapping;
+
+public sealed class ProductEntityConfiguration : IEntityTypeConfiguration<ProductEntity>
+{
+    public void Configure(EntityTypeBuilder<ProductEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired();
+
+        builder.HasOne(x => x.Studio)
+            .WithMany(s => s.Products)
+            .HasForeignKey(x => x.StudioId)
+            .IsRequired();
+
+        builder.HasMany(x => x.ProductUnits)
+            .WithOne(u => u.Product)
+            .HasForeignKey(u => u.ProductId)
+            .IsRequired();
+    }
+}
