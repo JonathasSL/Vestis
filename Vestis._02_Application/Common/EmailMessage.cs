da8141a9894a48517763e132602552966409fc19
JonathasSL <jonathasbent@gmail.com>
2026-02-12 16:36:16 -0300

    feat: EmailMessage #23

---
+++ Vestis._02_Application/Common/EmailMessage.cs
@@ -0,0 +1,17 @@
+﻿namespace Vestis._02_Application.Common;
+
+public class EmailMessage
+{
+    public IReadOnlyCollection<string> ToEmail { get; private set; }
+    public string Subject { get; private set; }
+    public string Body { get; private set; }
+    public bool IsHtml { get; private set; }
+
+    public EmailMessage(IEnumerable<string> toEmail, string subject, string body, bool isHtml = false)
+    {
+        ToEmail = toEmail.ToList().AsReadOnly();
+        Subject = subject;
+        Body = body;
+        IsHtml = isHtml;
+    }
+}
