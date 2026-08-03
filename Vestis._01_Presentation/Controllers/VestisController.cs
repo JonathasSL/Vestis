aa62ef561b8b1df2643000ed92cf8e52598f5a72
JonathasSL <jonathasbent@gmail.com>
2026-08-03 19:05:24 -0300

    feat: Novo metodo na vestiscontroller para centralizar o resultado baseado no CommandResult

--- Vestis._01_Presentation/Controllers/VestisController.cs
+++ Vestis._01_Presentation/Controllers/VestisController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Vestis._02_Application.Common;
 
 namespace Controllers;
 
@@ -6,4 +7,18 @@ namespace Controllers;
 [Route("api/[controller]/[Action]")]
 public class VestisController : ControllerBase
 {
+	protected IActionResult FromResult<T>(CommandResult<T> result)
+	{
+		switch (result.Status)
+		{
+			case CommandResultStatus.Success:
+				return Ok(result);
+			case CommandResultStatus.NotFound:
+				return NotFound(result);
+			case CommandResultStatus.ValidationFailure:
+				return BadRequest(result);
+			default:
+				return StatusCode(500, result);
+		}		
+	}
 }
