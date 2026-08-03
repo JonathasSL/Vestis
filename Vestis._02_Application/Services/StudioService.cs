de32aae71914e7c565fe18431b2c1acd2571714b
JonathasSL <jonathasbent@gmail.com>
2026-08-03 19:22:39 -0300

    fix: removing commandresult Failure as exception as command result is meant for user error

--- Vestis._02_Application/Services/StudioService.cs
+++ Vestis._02_Application/Services/StudioService.cs
@@ -16,16 +16,13 @@ namespace Vestis._02_Application.Services;
 
 public class StudioService : CRUDService<StudioModel, StudioEntity, Guid>, IStudioService
 {
-    private readonly IStudioRepository _repository;
     public StudioService(
         IMapper mapper,
         IMediator mediator,
         BusinessNotificationContext businessNotificationContext,
         ILogger<StudioService> logger, 
         IStudioRepository repository) : base(mapper, mediator, businessNotificationContext, logger, repository)
-    {
-        _repository = repository;
-    }
+    { }
 
     public async Task<CommandResult<StudioModel>> Create(Guid contextUser, StudioModel model)
     {
@@ -40,20 +37,19 @@ public class StudioService : CRUDService<StudioModel, StudioEntity, Guid>, IStud
                 addressCommand = new (
                     model.Address.Street,
                     model.Address.Number,
+                    model.Address.Complement,
                     model.Address.Neighborhood,
                     model.Address.City,
                     model.Address.State,
                     model.Address.ZipCode,
-                    model.Address.Neighborhood,
                     model.Address.Country
                 );
             }
 
             var studioCommand = new CreateStudioCommand(userId, model.Name, model.ContactEmail, model.PhoneNumber, addressCommand);
-            
+
             var entity = await _mediator.Send(studioCommand);
             result = _mapper.Map<StudioModel>(entity);
-
 
             if (_businessNotificationContext.HasNotifications)
                 return CommandResult<StudioModel>.Failure("Houve um erro ao criar seu ateliê.", _businessNotificationContext.Notifications.ToList());
@@ -63,11 +59,9 @@ public class StudioService : CRUDService<StudioModel, StudioEntity, Guid>, IStud
         catch (Exception e)
         {
             _logger.LogError(e.ExceptionStack(out _));
-
-            return CommandResult<StudioModel>.Failure( "Houve um erro ao criar seu ateliê. Verifique suas informações e tente novamente", _businessNotificationContext.Notifications.ToList());
+            throw;
         }
     }
-
 
     public CommandResult<List<StudioSummaryModel>> GetStudiosByUserId(Guid userId, CancellationToken cancellationToken)
     {
